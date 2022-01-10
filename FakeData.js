90cd48b08ff1ecbf2bc5d300e7412e4be71696fc
tom318267 <ogar318267@gmail.com>
2022-01-10 12:31:18 -0500

    first commit

---
+++ FakeData.js
@@ -0,0 +1,49 @@
+const UserList = [
+  {
+    id: 1,
+    name: "John",
+    username: "john",
+    age: 20,
+    nationality: "CANADA",
+    friends: [
+      {
+        id: 2,
+        name: "Pedro",
+        username: "PedroTech",
+        age: 20,
+        nationality: "BRAZIL",
+      },
+    ],
+  },
+
+  {
+    id: 2,
+    name: "Pedro",
+    username: "PedroTech",
+    age: 20,
+    nationality: "BRAZIL",
+  },
+];
+
+const MovieList = [
+  {
+    id: 1,
+    name: "Avengers Endgame",
+    yearOfPublication: 2019,
+    isInTheaters: true,
+  },
+  {
+    id: 2,
+    name: "Interstellar",
+    yearOfPublication: 2007,
+    isInTheaters: true,
+  },
+  {
+    id: 3,
+    name: "Superbad",
+    yearOfPublication: 2009,
+    isInTheaters: true,
+  },
+];
+
+module.exports = { UserList, MovieList };
